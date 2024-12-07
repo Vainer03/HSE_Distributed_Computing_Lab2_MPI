e6cca03852c1dafb45d642e4bf52858bd046c1ff
Anastasiia <avainer03@gmail.com>
2024-12-08 02:32:51 +0300

    Add task 1.2

--- src/task_1.c
+++ src/task_1.c
@@ -108,6 +108,86 @@ void matrix_vector_multiplication_row_split(int comm_rank, int comm_size, int n)
     free(receive_displacements);
 }
 
+void matrix_vector_multiplication_column_split(int comm_rank, int comm_size, int n) {
+    int *matrix = (int *)malloc(n * n * sizeof(int));
+    int *vector = (int *)malloc(n * sizeof(int));
+    int *result = (int *)malloc(n * sizeof(int));
+
+    int local_columns = n / comm_size + n % comm_size;
+    int vector_size = (comm_rank == comm_size - 1) ? local_columns : (n / comm_size);
+    if (local_columns <= vector_size) {
+        local_columns = vector_size;
+    }
+
+    int *local_matrix = (int *)malloc(local_columns * n * sizeof(int));
+    int *local_vector = (int *)malloc(vector_size * sizeof(int));
+
+    if (comm_rank == 0) {
+        initialize_vector(n * n, matrix);
+        initialize_vector(n, vector);
+        print_matrix(n, matrix);
+        print_vector(n, vector);
+    }
+
+    int *send_counts = (int *)malloc(comm_size * sizeof(int));
+    int *send_displacements = (int *)malloc(comm_size * sizeof(int));
+    int *receive_counts = (int *)malloc(comm_size * sizeof(int));
+
+    for (int i = 0; i < comm_size; i++) {
+        send_counts[i] = n / comm_size;
+        if (i == comm_size - 1) {
+            send_counts[i] += n % comm_size;
+        }
+        send_displacements[i] = i * n / comm_size;
+    }
+
+    MPI_Scatterv(vector, send_counts, send_displacements, MPI_INT, local_vector, vector_size, MPI_INT, 0, MPI_COMM_WORLD);
+
+    MPI_Datatype MPI_column, MPI_resized_column;
+
+    MPI_Type_vector(n, 1, n, MPI_INT, &MPI_column);
+    MPI_Type_create_resized(MPI_column, 0, sizeof(int), &MPI_resized_column);
+    MPI_Type_commit(&MPI_resized_column);
+
+    MPI_Scatterv(matrix, send_counts, send_displacements, MPI_resized_column, local_matrix, vector_size * n, MPI_INT, 0, MPI_COMM_WORLD);
+
+    MPI_Type_free(&MPI_resized_column);
+
+    int *local_result = (int *)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++) {
+        local_result[i] = 0;
+    }
+
+    for (int i = 0; i < comm_size; i++) {
+        receive_counts[i] = n;
+    }
+
+    for (int i = 0; i < vector_size; i++) {
+        for (int j = 0; j < n; j++) {
+            local_result[j] += local_matrix[i * n + j] * local_vector[i];
+        }
+    }
+
+    MPI_Reduce_scatter(local_result, result, receive_counts, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+
+    if (comm_rank == 0) {
+        printf("\nResult:\n");
+        for (int i = 0; i < n; i++) {
+            printf("%d ", result[i]);
+        }
+    }
+
+    free(matrix);
+    free(vector);
+    free(result);
+    free(local_matrix);
+    free(local_vector);
+    free(local_result);
+    free(send_counts);
+    free(send_displacements);
+    free(receive_counts);
+}
+
 int main(int argc, char** argv) {
     int comm_rank, comm_size;
     double start, finish;
@@ -140,7 +220,8 @@ int main(int argc, char** argv) {
     }
 
     start = MPI_Wtime();
-    matrix_vector_multiplication_row_split(comm_rank, comm_size, n_dim);
+    //matrix_vector_multiplication_row_split(comm_rank, comm_size, n_dim);
+    matrix_vector_multiplication_column_split(comm_rank, comm_size, n_dim);
     finish = MPI_Wtime();
 
     if (comm_rank == 0) {
@@ -152,4 +233,6 @@ int main(int argc, char** argv) {
 }
 
 #endif
+
+
 
